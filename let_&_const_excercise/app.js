3e1d942e6738bdc1810e3be933c0ebbed5fe8cb3
dwoodsteinhoff <119462719+dwoodsteinhoff@users.noreply.github.com>
2023-07-20 19:02:29 -0400

    Add files via upload

---
+++ let_&_const_excercise/app.js
@@ -0,0 +1,17 @@
+const PI = 3.14159
+
+//difference between var and let ?
+
+// you can reassign and redeclare var but you can not redeclare let. you can access a hoisted value using var and can't for let.
+
+//difference between var and const ?
+
+// you can reassign and redeclare var but you can not redeclare or reassign const. you can access a hoisted value using var and can't for const.
+
+//difference between  let and const?
+
+// you can reassign let but you cannot reassign const. you can't redeclare either.
+
+//what is hoisting?
+
+//the variable var is lifted to the top of whatever scope they are in. you can access a var value before it is used because it is hoisted. The value is just undefined.
